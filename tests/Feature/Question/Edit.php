21e2c30aff2a32e9db5c9050d33a669730b530c6
Maycon Queiroz <fmaycon@gmail.com>
2024-02-25 10:31:44 -0300

    HEY-13: feat(question): ensure that  only person who has created the question can update the question

--- tests/Feature/Question/Edit.php
+++ tests/Feature/Question/Edit.php
@@ -23,3 +23,33 @@ it('should return a view', function () {
     get(route('question.edit', $question))
         ->assertViewIs('question.edit');
 });
+
+it('should make sure that only question with status Draft can be edited', function () {
+    $user             = User::factory()->create();
+    $questionNotDraft = Question::factory()->for($user, 'createdBy')->create(['draft' => false]);
+    $questionDraft    = Question::factory()->for($user, 'createdBy')->create();
+
+    actingAs($user);
+
+    get(route('question.edit', $questionNotDraft))
+        ->assertForbidden();
+
+    get(route('question.edit', $questionDraft))
+        ->assertRedirect();
+});
+
+it('should make sure that only person who has created the question can update the question', function () {
+    $rightUser = User::factory()->create();
+    $wrongUser = User::factory()->create();
+    $question  = Question::factory()->create(['created_by' => $rightUser->id]);
+
+    actingAs($wrongUser);
+
+    get(route('question.edit', $question->id))
+        ->assertForbidden();
+
+    actingAs($rightUser);
+
+    get(route('question.edit', $question->id))
+        ->assertViewIs('question.edit');
+});
